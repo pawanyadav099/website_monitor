a3c393c6fc7b1c0c23490a4791e65964aa817694
pawanyadav099 <152092321+pawanyadav099@users.noreply.github.com>
2025-06-15 21:04:05 +0530

    Update url.py

--- url.py
+++ url.py
@@ -9,7 +9,7 @@ URLS = [
             "https://ssc.gov.in/home/notice-board",
             #Sikkim
             "https://spsc.sikkim.gov.in/Advertisement.html",
-            "spsc.sikkim.gov.in/Notices.html",
+            "https://spsc.sikkim.gov.in/Notices.html",
             "https://www.sikkim.gov.in/media/employment-news",
             "https://hcs.gov.in/hcs/recruitment",
             #Tamilnadu
