047d9e239fdedce9afb1d80f90b6fc800a6d621f
pawanyadav099 <152092321+pawanyadav099@users.noreply.github.com>
2025-06-16 00:15:22 +0530

    Update url.py

--- url.py
+++ url.py
@@ -1,6 +1,4 @@
 # url.py
 URLS = [
-          "https://websitenew.tspsc.gov.in/notifications"
-          "https://www.odishagovtjob.in/?m=1",
-          "https://odisha.gov.in/publication/advertisement"
+          
         ]
