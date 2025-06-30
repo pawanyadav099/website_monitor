907530f5a20f7b0704e0e79de226740877df1d06
pawanyadav099 <152092321+pawanyadav099@users.noreply.github.com>
2025-06-30 11:37:20 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,4 +1,9 @@
 # url.py
 urls = [
           "https://ssc.nic.in/"
+          #Sikkim
+          "https://spsc.sikkim.gov.in/Advertisement.html",
+          "spsc.sikkim.gov.in/Notices.html",
+          "https://www.sikkim.gov.in/media/employment-news",
+          "https://hcs.gov.in/hcs/recruitment",
         ]
